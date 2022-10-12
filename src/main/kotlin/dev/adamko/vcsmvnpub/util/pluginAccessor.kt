ca3fbbdd99dab9a781c0b64969aab1ab9d1e83ce
Adam Semenenko <adam.semenenko@gmail.com>
2022-10-12 11:39:55 +0200

    change pluginAccessor to function, to avoid clash with auto-generated accessor

--- src/main/kotlin/dev/adamko/vcsmvnpub/util/pluginAccessor.kt
+++ src/main/kotlin/dev/adamko/vcsmvnpub/util/pluginAccessor.kt
@@ -2,13 +2,13 @@
 
 package dev.adamko
 
-//import org.gradle.plugin.use.PluginDependenciesSpec
-//import org.gradle.plugin.use.PluginDependencySpec
-//
-///**
-// * The Gradle plugin implemented by [dev.adamko.vcsmvnpub.VcsMvnPublishPlugin].
-// *
-// * @see dev.adamko.vcsmvnpub.VcsMvnPublishPlugin
-// */
-//inline val PluginDependenciesSpec.`vcs-mvn-publish`: PluginDependencySpec
-//  get() = id("dev.adamko.vcs-mvn-publish")
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
+
+/**
+ * The Gradle plugin implemented by [dev.adamko.vcsmvnpub.VcsMvnPublishPlugin].
+ *
+ * @see dev.adamko.vcsmvnpub.VcsMvnPublishPlugin
+ */
+fun PluginDependenciesSpec.`vcs-mvn-publish`(): PluginDependencySpec =
+  id("dev.adamko.vcs-mvn-publish")
