018a21a3927579574884708230998804cd7b6080
Adam <897017+aSemy@users.noreply.github.com>
2023-01-13 21:26:10 +0100

    disable plugin accessor

--- src/main/kotlin/dev/adamko/vcsmvnpub/util/pluginAccessor.kt
+++ src/main/kotlin/dev/adamko/vcsmvnpub/util/pluginAccessor.kt
@@ -1,14 +1,14 @@
 @file:Suppress("PackageDirectoryMismatch", "ObjectPropertyName")
 
-package dev.adamko
-
-import org.gradle.plugin.use.PluginDependenciesSpec
-import org.gradle.plugin.use.PluginDependencySpec
-
-/**
- * The Gradle plugin implemented by [dev.adamko.vcsmvnpub.VcsMvnPublishPlugin].
- *
- * @see dev.adamko.vcsmvnpub.VcsMvnPublishPlugin
- */
-fun PluginDependenciesSpec.`vcs-mvn-publish`(): PluginDependencySpec =
-  id("dev.adamko.vcs-mvn-publish")
+//package dev.adamko
+//
+//import org.gradle.plugin.use.PluginDependenciesSpec
+//import org.gradle.plugin.use.PluginDependencySpec
+//
+///**
+// * The Gradle plugin implemented by [dev.adamko.vcsmvnpub.VcsMvnPublishPlugin].
+// *
+// * @see dev.adamko.vcsmvnpub.VcsMvnPublishPlugin
+// */
+//fun PluginDependenciesSpec.`vcs-mvn-publish`(): PluginDependencySpec =
+//  id("dev.adamko.vcs-mvn-publish")
